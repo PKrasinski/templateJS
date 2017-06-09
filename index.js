1f23db0c06470852e215d36814ccff049f4f1883
Przemysław <przemyslaw.rekinysukcesu@gmail.com>
2017-06-09 10:01:34 +0200

    Add files watcher

--- index.js
+++ index.js
@@ -11,3 +11,16 @@ const __config = require(__path + '/templateJS.json');
 const generator = new Generator(__path, __config);
 
 generator.getNewFileContent();
+
+var watch = require('watch')
+watch.watchTree(__path, function (f, curr, prev) {
+  if (typeof f == "object" && prev === null && curr === null) {
+    generator.getNewFileContent();
+  } else if (prev === null) {
+    generator.getNewFileContent();
+  } else if (curr.nlink === 0) {
+    generator.getNewFileContent();
+  } else {
+    generator.getNewFileContent();
+  }
+})
